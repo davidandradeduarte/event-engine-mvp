c1d6cccb7134fed084ab8bc3dafe0a06a1d7c9a7
vetras <{ID}+{username}@users.noreply.github.com>
2018-07-11 17:13:48 +0100

    fix console keys

--- EventsEngine_MVP/MT-Processor/MassTransitConfigurator.cs
+++ EventsEngine_MVP/MT-Processor/MassTransitConfigurator.cs
@@ -4,12 +4,12 @@ using MT_Common;
 
 namespace MT_Processor
 {
-    public class Processor : IDisposable
+    public class MassTransitConfigurator : IDisposable
     {
         private readonly IBusControl bus;
         private readonly CreatedFooConsumer consumer = new CreatedFooConsumer();
 
-        public Processor()
+        public MassTransitConfigurator()
         {
             bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
             {
@@ -23,12 +23,16 @@ namespace MT_Processor
                 {
                     // ep.Handler<CreatedFooConsumer>(context => Console.Out.WriteLineAsync($" yololo Received: {context.Message}"));
 
+                    // we register an instance so that we can send "sleep" "error" calls
+                    // normaly, we would leave the consumer class instance management to the MassTransit framework
                     //ep.Consumer<CreatedFooConsumer>();
                     ep.Instance(consumer);
-
                 });
             });
+        }
 
+        public void StartListening()
+        {
             bus.Start();
             Console.WriteLine("");
             Console.WriteLine($"Listening for {nameof(CreatedFoo)} Events...");
--- EventsEngine_MVP/MT-Processor/Program.cs
+++ EventsEngine_MVP/MT-Processor/Program.cs
@@ -1,41 +1,42 @@
 ﻿using System;
 using System.Threading.Tasks;
-using MassTransit;
-using MassTransit.RabbitMqTransport;
 
 namespace MT_Processor
 {
     public class Program
     {
-        private static Processor _processor;
+        private static MassTransitConfigurator _service;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Consumer here!");
-            Console.WriteLine("Press 'q' anytime to quit");
-            Console.WriteLine("Press 'e' anytime to simulate an error while processing the next message");
-            Console.WriteLine("Press 's' anytime to toogle sleep mode");
-            Console.WriteLine("");
 
             var taskKeys = new Task(ReadKeys);
 
             taskKeys.Start();
 
-            _processor = new Processor();
+            using (_service = new MassTransitConfigurator())
+            {
+                _service.StartListening();
 
-            Task.WaitAll(taskKeys);
-
-            _processor?.Dispose();
+                Task.WaitAll(taskKeys);
+            }
         }
 
         // https://stackoverflow.com/questions/5891538/listen-for-key-press-in-net-console-app#5891612
         private static void ReadKeys()
         {
+            Console.WriteLine("Press 'q' anytime to quit");
+            Console.WriteLine("Press 'e' anytime to simulate an error while processing the next message");
+            Console.WriteLine("Press 's' anytime to toogle sleep mode");
+            Console.WriteLine("");
+
             while (!Console.KeyAvailable)
             {
                 var key = Console.ReadKey(true);
                 if (key.Key == ConsoleKey.Q) break;
-                if (key.Key == ConsoleKey.E) _processor?.ToggleErrorSimulation();
-                if (key.Key == ConsoleKey.S) _processor?.ToggleSleepMode();
+                if (key.Key == ConsoleKey.E) _service?.ToggleErrorSimulation();
+                if (key.Key == ConsoleKey.S) _service?.ToggleSleepMode();
             }
         }
     }
